e23f67537ea1ad34eb3d2d170c2c5df0a85296fd
mito01100010 <dimitar.pashkulev@gmail.com>
2021-06-02 23:49:02 +0300

    Main screen

---
+++ app/src/main/java/com/example/jobtimer/activities/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.jobtimer.activities;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.jobtimer.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timerText;
+    Button stopStartButton;
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+
+    boolean timerStarted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner taskSpinner = (Spinner) findViewById(R.id.tasksSpinner);
+        timerText = (TextView) findViewById(R.id.timerText);
+        stopStartButton = (Button) findViewById(R.id.startStopButton);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.tasks));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        taskSpinner.setAdapter(adapter);
+        timer = new Timer();
+
+    }
+
+    public void resetButton(View view) {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Are you sure you finished your task?");
+        resetAlert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(timerTask != null){
+                    timerTask.cancel();
+                    setButtonUI(stopStartButton, "Start");
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0,0,0));
+                    workFinishedDialog();
+                }
+            }
+        });
+
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        resetAlert.show();
+    }
+
+
+    public void startStopButton(View view) {
+
+        if(timerStarted == false){
+            timerStarted = true;
+            setButtonUI(stopStartButton, "Pause Work");
+
+            startTimer();
+        }else{
+            timerStarted = false;
+            setButtonUI(stopStartButton, "Continue Work");
+
+            timerTask.cancel();
+        }
+    }
+
+    private void setButtonUI(TextView stopStartButton, String start) {
+        stopStartButton.setText(start);
+        //може и смяна на цвят?
+    }
+
+    private void startTimer() {
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        setButtonUI(timerText, getTimerText());
+                    }
+                });
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0,1000);
+    }
+
+    private String getTimerText() {
+
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds,minutes,hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours) {
+
+        return String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+    }
+
+    private void workFinishedDialog() {
+        WorkFinishedDialog wfd = new WorkFinishedDialog();
+        wfd.show(getSupportFragmentManager(), "Work finished dialog");
+    }
+
+}
---
+++ app/src/main/java/com/example/jobtimer/activities/WorkFinishedDialog.java
@@ -0,0 +1,42 @@
+package com.example.jobtimer.activities;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.example.jobtimer.R;
+
+public class WorkFinishedDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_work_finished_dialog, null);
+
+        builder.setView(view)
+                .setTitle("NOTE")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //add note in the db
+                    }
+                });
+        return builder.create();
+    }
+}
